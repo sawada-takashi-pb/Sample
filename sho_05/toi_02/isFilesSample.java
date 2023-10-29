98eb9bcda79cb6773dfb3e6450e9dbd1e9b5c110
sawada-takashi-pb <yahuokusawa@yahoo.co.jp>
2023-10-29 14:03:08 +0900

    Update isFilesSample.java

--- sho_05/toi_02/isFilesSample.java
+++ sho_05/toi_02/isFilesSample.java
@@ -1,9 +1,20 @@
 package sho_05.toi_02;
 
+import java.io.File;
+
 public class isFilesSample {
 
 	public static void main(String[] args) {
-
+		File rootDir = new File(".");
+		showTree(rootDir);
+	}
+	
+	private static void showTree(File dir) {
+		File[] files = dir.listFiles();
+		for(File file : files) {
+			System.out.println(file.getName());
+		}
+		
 	}
 
 }
